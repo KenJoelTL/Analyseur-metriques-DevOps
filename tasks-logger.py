829bdc13e74b0809270ffc2cd21c63d9527a354f
Ken Joel L <joellutumba@hotmail.com>
2022-02-05 20:02:59 -0500

    valeur du fichier de config dans le script

--- tasks-logger.py
+++ tasks-logger.py
@@ -1,31 +1,26 @@
 import time
-import os
 import json
 import requests
 from datetime import datetime
-testUrl = "https://api.github.com/microsoft/PowerToys/projects"
-baseUrl_ = 'https://api.github.com/users/pascal1796/'
-baseUrl = 'https://api.github.com/'
-repo_base_url = 'https://api.github.com/repos/KenJoelTL/LOG680-tp01-eq08/'
-api_base = 'https://api.github.com/user/repos'
-headers = {"authorization": "token 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"}
-project_id = 14032098
+import config
 
 
 def getKanbanTaskList():
-    r = requests.get(url=baseUrl+"projects/" +
-                     str(project_id)+"/columns", headers=headers)
+    r = requests.get(url=config.baseUrl+"projects/" +
+                     str(config.project_id)+"/columns", headers=config.headers)
     if r.status_code == 200:
         data = r.json()
         tasks = {}
         for columns in data:
             col_title = columns["name"]
             tasks[col_title] = []
-            r = requests.get(url=columns["url"]+"/cards", headers=headers)
+            r = requests.get(url=columns["url"] +
+                             "/cards", headers=config.headers)
             if r.status_code == 200:
                 res = r.json()
                 for cards in res:
-                    r = requests.get(url=cards["content_url"], headers=headers)
+                    r = requests.get(
+                        url=cards["content_url"], headers=config.headers)
                     if r.status_code == 200:
                         card = r.json()
                         card_to_save = {}
